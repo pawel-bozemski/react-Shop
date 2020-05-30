76ced9b223edabc30ca6be9a771e22d2fd5cf874
Pawel Bozemski <paw.boz@gmail.com>
2020-05-30 12:40:19 +0200

    fix errors

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,6 +1,6 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
 
 import { initialState } from './initialState';
 import { reducer as productReducer } from './productRedux';
